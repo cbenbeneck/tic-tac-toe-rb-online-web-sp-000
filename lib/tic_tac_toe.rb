102e88b8557ff39f2b9b66044873925fa8c06825
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-12 14:40:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ end
 def position_taken?(board, index)
   if board[index] == "X"
     return true
-  elsif board[index]  == "O"
+  elsif board[index] == "O"
     return true
 else board[index] == false
   end
